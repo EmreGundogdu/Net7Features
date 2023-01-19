cc7b67e17a82bfbf92f6262513f6bbe274d8f40a
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-19 22:37:35 +0300

    Attributes added

--- RateLimiting/Controllers/ValuesController.cs
+++ RateLimiting/Controllers/ValuesController.cs
@@ -9,6 +9,9 @@ namespace RateLimiting.Controllers
     [EnableRateLimiting("Concurrency")] //basic isimli rate limit çalışıcak
     public class ValuesController : ControllerBase
     {
+        //EnableRateLimiting Attribute: Controller yahut action seviyesinde istenilen politikada rate limiti devreye sokmamızı sağlayan bir attributedir
+        //DisableRateLimiting Attribute: Controller seviyesinde devreye sokulmuş bir rate limit politikasının action seviyesinde pasifleştirilmesini sağlayan bir attributedir.
+
         public async Task<IActionResult> Get() //async metodlar da conccurrency rate limiter geçerlidir.
         {
             return Ok();
